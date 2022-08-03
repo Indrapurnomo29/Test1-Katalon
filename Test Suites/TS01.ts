f0f2d1600e19131068f59d7eab167e9eaf2b251e
Indra Purnomo <indpurno@gmail.com>
2022-08-03 08:08:29 +0700

    First commit

---
+++ Test Suites/TS01.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>79fcc68e-28cb-4211-9e4f-2e43fa29ddf2</testSuiteGuid>
+   <testCaseLink>
+      <guid>81ef15b2-f7de-4b32-b441-2156a7f9290f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01-ValidationResponseAndVerifyJSONValue</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73e70460-e601-49ef-a7fa-2fd9bca81957</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02-ValidationResponseSingleUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>015cccb0-6937-4aff-9733-16684245237c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03-ValidationResponseUpdateUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a03e7661-619c-4545-9658-c74b48a24c87</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04-ValidatonResponseRegisterSuccessful</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
